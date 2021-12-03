026224b1050db9c5c1e7fe2fd38361b8cedcf93c
svenrdz <svenbmr@gmail.com>
2021-12-04 00:27:10 +0100

    day2

---
+++ day2.nim
@@ -0,0 +1,46 @@
+import utils, strutils
+
+type
+  Direction = enum
+    Forward = "forward"
+    Down = "down"
+    Up = "up"
+  Position = object
+    x, y: int
+    aim: int
+  Move = tuple
+    direction: Direction
+    value: int
+
+proc parse(line: string): Move =
+  result.direction = parseEnum[Direction](line.split()[0])
+  result.value = parseInt(line.split()[1])
+
+proc moveBy(submarine: var Position, line: string) =
+  let move = parse(line)
+  case move.direction
+  of Forward:
+    submarine.x += move.value
+  of Down:
+    submarine.y += move.value
+  of Up:
+    submarine.y -= move.value
+
+proc aimBy(submarine: var Position, line: string) =
+  let move = parse(line)
+  case move.direction
+  of Forward:
+    submarine.x += move.value
+    submarine.y += submarine.aim * move.value
+  of Down:
+    submarine.aim += move.value
+  of Up:
+    submarine.aim -= move.value
+
+day 2:
+  var submarine1, submarine2: Position
+  for line in read(file):
+    submarine1.moveBy(line)
+    submarine2.aimBy(line)
+  part1 = submarine1.x * submarine1.y
+  part2 = submarine2.x * submarine2.y
